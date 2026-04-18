244191d70e34f92850bcfc7f2c753912b6f6d56a
Bobr2610 <misha.sedov07@gmail.com>
2026-04-18 10:11:41 +0300

    Splau imp;imantation

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -11,17 +11,88 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (child != null && IsAttachedToCurrentTree(child))
+            Splay(child);
+        else if (parent != null && IsAttachedToCurrentTree(parent))
+            Splay(parent);
     }
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        BstNode<TKey, TValue>? node = FindNode(key);
+        if (node != null)
+        {
+            value = node.Value;
+            Splay(node);
+            return true;
+        }
+
+        value = default;
+        return false;
+    }
+
+    public override bool ContainsKey(TKey key)
+    {
+        BstNode<TKey, TValue>? node = FindNode(key);
+        if (node == null)
+        {
+            return false;
+        }
+
+        Splay(node);
+        return true;
     }
     
+    private void Splay(BstNode<TKey, TValue> node)
+    {
+        while (node.Parent != null)
+        {
+            BstNode<TKey, TValue> parent = node.Parent;
+            BstNode<TKey, TValue>? grandParent = parent.Parent;
+
+            if (grandParent == null)
+            {
+                if (parent.Left == node)
+                    RotateRight(parent);
+                else
+                    RotateLeft(parent);
+            }
+            else if (parent.Left == node && grandParent.Left == parent)
+            {
+                RotateRight(grandParent);
+                RotateRight(parent);
+            }
+            else if (parent.Right == node && grandParent.Right == parent)
+            {
+                RotateLeft(grandParent);
+                RotateLeft(parent);
+            }
+            else if (parent.Left == node && grandParent.Right == parent)
+            {
+                RotateRight(parent);
+                RotateLeft(grandParent);
+            }
+            else
+            {
+                RotateLeft(parent);
+                RotateRight(grandParent);
+            }
+        }
+    }
+
+    private bool IsAttachedToCurrentTree(BstNode<TKey, TValue> node)
+    {
+        BstNode<TKey, TValue> current = node;
+        while (current.Parent != null)
+        {
+            current = current.Parent;
+        }
+
+        return ReferenceEquals(current, Root);
+    }
 }
